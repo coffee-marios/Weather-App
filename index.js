33eb053bdf1fd1b8df0882bea748289e0fcce946
coffee-marios <mark_theo@yahoo.com>
2024-05-13 13:44:39 +0300

    Fetch and show data about the weather for the next day

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const myLocation = document.getElementById("myLocation");
 const myCondition = document.getElementById("myCondition");
 const iconWeather = document.getElementById("icon-weather");
 
-async function get_weather() {
+//
+
+const myTemperature_tom = document.getElementById("myTemperature_tom");
+const iconWeather_tom = document.getElementById("icon-weather_tom");
+const myCondition_tom = document.getElementById("myCondition_tom");
+
+//
+
+async function get_weather_today() {
   try {
     const response = await fetch(
       "http://api.weatherapi.com/v1/current.json?key=3e09bdd8cabd408d9ec63431240805&q=London&aqi=no",
@@ -12,17 +20,37 @@ async function get_weather() {
         mode: "cors",
       }
     );
-    const data_location = await response.json();
-    console.log(data_location.current.temp_c);
-    const temperature_celsius = data_location.current.temp_c;
-    iconWeather.src = data_location.current.condition.icon;
-    myCondition.textContent = data_location.current.condition.text;
-    myTemperature.innerHTML =
-      `Temperature (Celsius): ${temperature_celsius}` + degrees_char;
-    myLocation.textContent = data_location.location.name;
+
+    const data_weather = await response.json();
+    // console.log(data_weather.current.temp_c);
+    const temperature_celsius = data_weather.current.temp_c;
+    iconWeather.src = data_weather.current.condition.icon;
+    myCondition.textContent = data_weather.current.condition.text;
+    myTemperature.textContent = temperature_celsius + degrees_char + " C";
+    myLocation.textContent = data_weather.location.name;
   } catch (error) {
     console.log(error);
   }
 }
 
-get_weather();
+async function get_weather_tomorrow() {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=3e09bdd8cabd408d9ec63431240805&q=London&days=1&aqi=no&alerts=no
+      `
+    );
+    const data_weather_tom = await response.json();
+    console.log(data_weather_tom);
+    const tomorrow_forecast = data_weather_tom.forecast.forecastday[0].day;
+
+    iconWeather_tom.src = tomorrow_forecast.condition.icon;
+    myTemperature_tom.textContent =
+      tomorrow_forecast.avgtemp_c + degrees_char + " C";
+    myCondition_tom.textContent = tomorrow_forecast.condition.text;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+get_weather_today();
+get_weather_tomorrow();
